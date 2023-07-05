25c33d680246ad6bee2a99cd55540bf464d8d661
Alan Mimms <alanmimms@lucidmotors.com>
2023-07-04 17:13:49 -0700

    Make options more consistent

--- sim.js
+++ sim.js
@@ -10,10 +10,10 @@ CMDR
   .option('-a, --dump-ast', `Dump AST after parsing`)
   .option('-b, --dump-backplane', `Dump backplane slots and net name`)
   .option('-s, --dump-signals', `Dump signals list after parsing`)
+  .option('-n, --dump-nets', `Dump netlist`)
+  .option('-u, --dump-undriven', `Dump list of undriven nets`)
+  .option('-o, --dump-wire-or', `Dump list of wire-ORed nets`)
   .option('-v, --verbose-errors', `Give more info for errors`)
-  .option('-n, --check-nets', `Verify netlist`)
-  .option('-u, --check-undriven', `Show list of undriven nets`)
-  .option('-o, --check-wire-or', `Show list of wire-ORed nets`)
   .argument('<src>', `Source file to start parsing`)
   .action((src, options) => {
     options.src = src;
