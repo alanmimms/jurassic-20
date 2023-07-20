aa971eef142c124df999dba4f94f35236ebe27a2
Alan Mimms <alanmimms@lucidmotors.com>
2023-07-19 17:26:05 -0700

    Sort options alphabetically. Add options to contol new dump types.

--- sim.js
+++ sim.js
@@ -7,14 +7,16 @@ const {compile} = require('./compile');
 
 CMDR
   .showHelpAfterError()
-  .option('-t, --trace-parse', `Print trace while parsing netlist`)
   .option('-a, --dump-ast', `Dump AST after parsing`)
   .option('-b, --dump-backplane', `Dump backplane slots and net names`)
-  .option('-s, --dump-signals', `Dump signals list after parsing`)
   .option('-n, --dump-nets', `Dump netlist`)
-  .option('-u, --dump-undriven', `Dump list of undriven nets`)
   .option('-o, --dump-wire-or', `Dump list of wire-ORed nets`)
+  .option('-p, --dump-bp-pins', `Dump backplane pin assignments on each board`)
+  .option('-s, --dump-signals', `Dump signals list after parsing`)
+  .option('-t, --trace-parse', `Print trace while parsing netlist`)
+  .option('-u, --dump-undriven', `Dump list of undriven nets`)
   .option('-v, --verbose-errors', `Give more info for errors`)
+  .option('-w, --dump-wires', `Dump list of wires on each board`)
   .argument('[src]', `Source file to start parsing`, `kl10pv.backplane`)
   .action((src, options) => {
     options.src = src;
