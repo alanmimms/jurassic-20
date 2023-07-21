b14d9724f84f52539622c6ed4573e37b6eb895fa
Alan Mimms <alanmimms@lucidmotors.com>
2023-07-21 13:44:41 -0700

    Change meaning of -s to dump slot backplane pins

--- sim.js
+++ sim.js
@@ -13,7 +13,7 @@ CMDR
   .option('-n, --dump-nets', `Dump netlist`)
   .option('-o, --dump-wire-or', `Dump list of wire-ORed nets`)
   .option('-p, --dump-pins', `Dump board pins and backplane pins for each board`)
-  .option('-s, --dump-signals', `Dump signals list after parsing`)
+  .option('-s, --dump-slots', `Dump slot signals after parsing`)
   .option('-t, --trace-parse', `Print trace while parsing netlist`)
   .option('-u, --dump-undriven', `Dump list of undriven nets`)
   .option('-v, --verbose-errors', `Give more info for errors`)
