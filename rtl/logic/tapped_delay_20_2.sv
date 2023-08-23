29c06b85104ab78a1f7c1e9ef6c9609f93fe57fd
Alan Mimms <alanmimms@lucidmotors.com>
2023-08-23 14:30:31 -0700

    tapped Delay line 20 2-a

---
+++ rtl/logic/tapped_delay_20_2.sv
@@ -0,0 +1,12 @@
+module tapped_delay_20_2(input bit in, output bit t1,t2,t3,t4,t5,t6,t7,t8,t9, out);
+  assign t1 = in;
+  assign t2 = in;
+  assign t3 = in;
+  assign t4 = in;
+  assign t5 = in;
+  assign t6 = in;
+  assign t7 = in;
+  assign t8 = in;
+  assign t9 = in;
+  assign out = in;
+endmodule // tapped_delay_20_2
---
+++ rtl/tb/tapped_delay_20_2-tb.cc
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <iostream>
+#include <verilated.h>
+#include <verilated_vcd_c.h>
+#include "Vtapped_delay_20_2.h"
+
+vluint64_t simT = 0;
+
+#define ALL_BIT_FIELDS \
+  DO1(in)
+
+int main(int argc, char** argv, char** env) {
+
+#define DO1(F)	const vluint64_t F##bit = 1ull << Fbit++;
+  vluint64_t Fbit = 1;
+  ALL_BIT_FIELDS
+#undef DO1
+
+  static const unsigned MAX_SIM_TIME = (1ull << Fbit) + 3;
+  Vtapped_delay_20_2 *dut = new Vtapped_delay_20_2;
+
+  Verilated::traceEverOn(true);
+  VerilatedVcdC *m_trace = new VerilatedVcdC;
+  dut->trace(m_trace, 5);
+  m_trace->open("waveform.vcd");
+
+  while (simT < MAX_SIM_TIME) {
+#define DO1(F)	do {dut->F = !!(simT & F##bit); } while (0);
+    ALL_BIT_FIELDS
+#undef DO1
+    dut->eval();
+    m_trace->dump(simT);
+    simT++;
+  }
+
+  m_trace->close();
+  delete dut;
+  exit(EXIT_SUCCESS);
+}
