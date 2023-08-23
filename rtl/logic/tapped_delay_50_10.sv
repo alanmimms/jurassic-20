ac6818568c601bf3477697282375fda7d7079a83
Alan Mimms <alanmimms@lucidmotors.com>
2023-08-23 15:35:59 -0700

    tapped delay line

---
+++ rtl/logic/tapped_delay_50_10.sv
@@ -0,0 +1,7 @@
+module tapped_delay_50_10(input bit in, output bit t1,t2,t3,t4, out);
+  assign t1 = in;
+  assign t2 = in;
+  assign t3 = in;
+  assign t4 = in;
+  assign out = in;
+endmodule // tapped_delay_50_10
