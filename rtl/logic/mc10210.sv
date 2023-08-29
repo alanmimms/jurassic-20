b38ffcaa600de385688b0a0175e6269bff91b4a3
Alan Mimms <alanmimms@lucidmotors.com>
2023-08-28 17:12:26 -0700

    Consolidate use of <CV2> and <CR1> with CONSISTENT NAME for each use in each slot

--- rtl/logic/mc10210.sv
+++ rtl/logic/mc10210.sv
@@ -1,10 +1,10 @@
 module mc10210(input bit a1,a2,a3, b1,b2,b3,
 	       output bit qa1,qa2,qa3, qb1,qb2,qb3);
    
-  assign qa1 = a1|a2|a3;
-  assign qa2 = qa1;
-  assign qa3 = qa1;
-  assign qb1 = b1|b2|b3;
-  assign qb2 = qb1;
-  assign qb3 = qb1;
+  always_comb qa1 = a1|a2|a3;
+  always_comb qa2 = qa1;
+  always_comb qa3 = qa1;
+  always_comb qb1 = b1|b2|b3;
+  always_comb qb2 = qb1;
+  always_comb qb3 = qb1;
 endmodule // mc10210
