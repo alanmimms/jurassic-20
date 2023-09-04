4f8cb06efcde20c7136e88c1a7d56f59bcea9c4b
Alan Mimms <alanmimms@lucidmotors.com>
2023-09-04 15:59:28 -0700

    Debug

--- rtl/logic/mc10136.sv
+++ rtl/logic/mc10136.sv
@@ -10,22 +10,27 @@ module mc10136(input bit  d8, d4, d2, d1, nCryIn,
   // nCryIn overrides clk when in INC or DEC mode. This signal is the
   // real clock we have to pay attention to as a result.
   bit carryClk;
-  always_comb carryClk = incOrDec ? nCryIn : clk;
+
+  bit carryOut, carryIn;
+  assign carryIn = !nCryIn;
+
+  always_comb nCryOut = !carryOut;
+  always_comb carryClk = incOrDec ? clk : !nCryIn;
 
   always_comb begin
     unique case (sel[1:0])
-      2'b00: nCryOut = 1;			 // LOAD
-      2'b01: nCryOut = {q8,q4,q2,q1} == 4'b1111; // INC
-      2'b10: nCryOut = {q8,q4,q2,q1} == '0;	 // DEC
-      2'b11: nCryOut = 0;			 // HOLD
+      2'b00: carryOut = 1;			  // LOAD
+      2'b01: carryOut = {q8,q4,q2,q1} == 4'b1111; // INC
+      2'b10: carryOut = {q8,q4,q2,q1} == '0;	  // DEC
+      2'b11: carryOut = 0;			  // HOLD
     endcase
   end
 
   always_ff @(posedge carryClk) begin
     unique case (sel[1:0])
       2'b00: {q8,q4,q2,q1} <= {d8,d4,d2,d1};				// LOAD
-      2'b01: if (nCryIn) {q8,q4,q2,q1} <= {q8,q4,q2,q1} + 4'b0001;	// INC
-      2'b10: if (nCryIn) {q8,q4,q2,q1} <= {q8,q4,q2,q1} - 4'b0001;	// DEC
+      2'b01: if (carryIn) {q8,q4,q2,q1} <= {q8,q4,q2,q1} + 4'b0001;	// INC
+      2'b10: if (carryIn) {q8,q4,q2,q1} <= {q8,q4,q2,q1} - 4'b0001;	// DEC
       2'b11: ;								// HOLD
     endcase
   end
