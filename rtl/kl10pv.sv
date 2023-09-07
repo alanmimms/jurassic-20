71bea6253fe6f85557a614f3e06b876b344c40f2
Alan Mimms <alanmimms@lucidmotors.com>
2023-09-06 17:50:47 -0700

    Keep EBUS driving same data after current driver stops driving

--- rtl/kl10pv.sv
+++ rtl/kl10pv.sv
@@ -19,7 +19,7 @@ module kl10pv(input clk60);
   assign clk = clk60;
 
   iEBUS ebus();
-  fe_sim feSim (.EBUSdriver(feEBUSdriver), .clk(clk_10_11_clk_h), .*);
+  fe_sim feSim (.EBUSdriver(feEBUSdriver), .clk(!clk_10_11_clk_h), .*);
 
   apr34 apr_34(.EBUSdriver(aprEBUSdriver), .*);
   cac17 cac_17(.*);
@@ -132,41 +132,74 @@ module kl10pv(input clk60);
   assign ebus_pi06_e_h = ebus.pi[6];
   assign ebus_pi07_e_h = ebus.pi[7];
 
+  bit anyEBUSdriving;
+  assign anyEBUSdriving = feEBUSdriver.driving |
+			  aprEBUSdriver.driving |
+			  cclEBUSdriver.driving |
+			  ccwEBUSdriver.driving |
+			  chcEBUSdriver.driving |
+			  chxEBUSdriver.driving |
+			  clkEBUSdriver.driving |
+			  conEBUSdriver.driving |
+			  craEBUSdriver.driving |
+			  crm40EBUSdriver.driving |
+			  crm42EBUSdriver.driving |
+			  crm44EBUSdriver.driving |
+			  crm50EBUSdriver.driving |
+			  crm52EBUSdriver.driving |
+			  cshEBUSdriver.driving |
+			  ctlEBUSdriver.driving |
+			  edp39EBUSdriver.driving |
+			  edp41EBUSdriver.driving |
+			  edp43EBUSdriver.driving |
+			  edp49EBUSdriver.driving |
+			  edp51EBUSdriver.driving |
+			  edp53EBUSdriver.driving |
+			  irdEBUSdriver.driving |
+			  mbcEBUSdriver.driving |
+			  mbxEBUSdriver.driving |
+			  mbzEBUSdriver.driving |
+			  mclEBUSdriver.driving |
+			  mtrEBUSdriver.driving |
+			  picEBUSdriver.driving |
+			  scdEBUSdriver.driving |
+			  vmaEBUSdriver.driving;
+
   // Mux for EBUS data lines
-  always_comb
+  always_ff @(posedge anyEBUSdriving) begin
     unique case (1)
-      default: ebus.data = '0;
-      feEBUSdriver.driving:	ebus.data = feEBUSdriver.data;
-      aprEBUSdriver.driving:	ebus.data = aprEBUSdriver.data;
-      cclEBUSdriver.driving:	ebus.data = cclEBUSdriver.data;
-      ccwEBUSdriver.driving:	ebus.data = ccwEBUSdriver.data;
-      chcEBUSdriver.driving:	ebus.data = chcEBUSdriver.data;
-      chxEBUSdriver.driving:	ebus.data = chxEBUSdriver.data;
-      clkEBUSdriver.driving:	ebus.data = clkEBUSdriver.data;
-      conEBUSdriver.driving:	ebus.data = conEBUSdriver.data;
-      craEBUSdriver.driving:	ebus.data = craEBUSdriver.data;
-      crm40EBUSdriver.driving:	ebus.data = crm40EBUSdriver.data;
-      crm42EBUSdriver.driving:	ebus.data = crm42EBUSdriver.data;
-      crm44EBUSdriver.driving:	ebus.data = crm44EBUSdriver.data;
-      crm50EBUSdriver.driving:	ebus.data = crm50EBUSdriver.data;
-      crm52EBUSdriver.driving:	ebus.data = crm52EBUSdriver.data;
-      cshEBUSdriver.driving:	ebus.data = cshEBUSdriver.data;
-      ctlEBUSdriver.driving:	ebus.data = ctlEBUSdriver.data;
-      edp39EBUSdriver.driving:	ebus.data = edp39EBUSdriver.data;
-      edp41EBUSdriver.driving:	ebus.data = edp41EBUSdriver.data;
-      edp43EBUSdriver.driving:	ebus.data = edp43EBUSdriver.data;
-      edp49EBUSdriver.driving:	ebus.data = edp49EBUSdriver.data;
-      edp51EBUSdriver.driving:	ebus.data = edp51EBUSdriver.data;
-      edp53EBUSdriver.driving:	ebus.data = edp53EBUSdriver.data;
-      irdEBUSdriver.driving:	ebus.data = irdEBUSdriver.data;
-      mbcEBUSdriver.driving:	ebus.data = mbcEBUSdriver.data;
-      mbxEBUSdriver.driving:	ebus.data = mbxEBUSdriver.data;
-      mbzEBUSdriver.driving:	ebus.data = mbzEBUSdriver.data;
-      mclEBUSdriver.driving:	ebus.data = mclEBUSdriver.data;
-      mtrEBUSdriver.driving:	ebus.data = mtrEBUSdriver.data;
-      picEBUSdriver.driving:	ebus.data = picEBUSdriver.data;
-      scdEBUSdriver.driving:	ebus.data = scdEBUSdriver.data;
-      vmaEBUSdriver.driving:	ebus.data = vmaEBUSdriver.data;
-    endcase
+      feEBUSdriver.driving:	ebus.data <= feEBUSdriver.data;
+      aprEBUSdriver.driving:	ebus.data <= aprEBUSdriver.data;
+      cclEBUSdriver.driving:	ebus.data <= cclEBUSdriver.data;
+      ccwEBUSdriver.driving:	ebus.data <= ccwEBUSdriver.data;
+      chcEBUSdriver.driving:	ebus.data <= chcEBUSdriver.data;
+      chxEBUSdriver.driving:	ebus.data <= chxEBUSdriver.data;
+      clkEBUSdriver.driving:	ebus.data <= clkEBUSdriver.data;
+      conEBUSdriver.driving:	ebus.data <= conEBUSdriver.data;
+      craEBUSdriver.driving:	ebus.data <= craEBUSdriver.data;
+      crm40EBUSdriver.driving:	ebus.data <= crm40EBUSdriver.data;
+      crm42EBUSdriver.driving:	ebus.data <= crm42EBUSdriver.data;
+      crm44EBUSdriver.driving:	ebus.data <= crm44EBUSdriver.data;
+      crm50EBUSdriver.driving:	ebus.data <= crm50EBUSdriver.data;
+      crm52EBUSdriver.driving:	ebus.data <= crm52EBUSdriver.data;
+      cshEBUSdriver.driving:	ebus.data <= cshEBUSdriver.data;
+      ctlEBUSdriver.driving:	ebus.data <= ctlEBUSdriver.data;
+      edp39EBUSdriver.driving:	ebus.data <= edp39EBUSdriver.data;
+      edp41EBUSdriver.driving:	ebus.data <= edp41EBUSdriver.data;
+      edp43EBUSdriver.driving:	ebus.data <= edp43EBUSdriver.data;
+      edp49EBUSdriver.driving:	ebus.data <= edp49EBUSdriver.data;
+      edp51EBUSdriver.driving:	ebus.data <= edp51EBUSdriver.data;
+      edp53EBUSdriver.driving:	ebus.data <= edp53EBUSdriver.data;
+      irdEBUSdriver.driving:	ebus.data <= irdEBUSdriver.data;
+      mbcEBUSdriver.driving:	ebus.data <= mbcEBUSdriver.data;
+      mbxEBUSdriver.driving:	ebus.data <= mbxEBUSdriver.data;
+      mbzEBUSdriver.driving:	ebus.data <= mbzEBUSdriver.data;
+      mclEBUSdriver.driving:	ebus.data <= mclEBUSdriver.data;
+      mtrEBUSdriver.driving:	ebus.data <= mtrEBUSdriver.data;
+      picEBUSdriver.driving:	ebus.data <= picEBUSdriver.data;
+      scdEBUSdriver.driving:	ebus.data <= scdEBUSdriver.data;
+      vmaEBUSdriver.driving:	ebus.data <= vmaEBUSdriver.data;
+    endcase // unique case (1)
+  end // always_ff @ (posedge anyEBUSdriving)
   
 endmodule // kl10pv
