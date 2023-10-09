f76b4b394c3b00381b38909271bda75a36b7a44f
Alan Mimms <alanmimms@gmail.com>
2023-10-08 19:44:52 -0700

    Broken priority encoder in several ways

--- rtl/logic/mc10165.sv
+++ rtl/logic/mc10165.sv
@@ -2,21 +2,19 @@ module mc10165(input bit d0,d1,d2,d3,d4,d5,d6,d7,
 	       hold,
 	       output bit any, q4, q2, q1);
 
+  always_comb any = d0 | d1 | d2 | d3 | d4 | d5 | d6 | d7;
+
   always_latch
 
-    if (hold) begin
-      priority case ({d0,d1,d2,d3,d4,d5,d6,d7})
-	0: {q4,q2,q1} = 0;
-	1: {q4,q2,q1} = 1;
-	2: {q4,q2,q1} = 2;
-	3: {q4,q2,q1} = 3;
-	4: {q4,q2,q1} = 4;
-	5: {q4,q2,q1} = 5;
-	6: {q4,q2,q1} = 6;
-	7: {q4,q2,q1} = 7;
-      endcase // priority case ({d0,d1,d2,d3,d4,d5,d6,d7})
-
-      any = !|{d0,d1,d2,d3,d4,d5,d6,d7};
+    if (!hold) begin
+      if (d0) {q4,q2,q1} = 0;
+      else if (d1) {q4,q2,q1} = 1;
+      else if (d2) {q4,q2,q1} = 2;
+      else if (d3) {q4,q2,q1} = 3;
+      else if (d4) {q4,q2,q1} = 4;
+      else if (d5) {q4,q2,q1} = 5;
+      else if (d6) {q4,q2,q1} = 6;
+      else if (d7) {q4,q2,q1} = 7;
     end
 
 endmodule
