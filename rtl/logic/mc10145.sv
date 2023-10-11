20a321c57b2490d0900bb9b81642111cbd2069d7
Alan Mimms <alanmimms@gmail.com>
2023-10-10 20:02:39 -0700

    Still debugging CRA call/return

--- rtl/logic/mc10145.sv
+++ rtl/logic/mc10145.sv
@@ -4,28 +4,25 @@ module mc10145(input bit a0, a1, a2, a3,
 
   bit [3:0] ram[0:15];
   bit [3:0] addr;
-  bit cs, we, re;
+  bit cs, we;
   bit [3:0] q;
   bit [3:0] d;
 
   always_comb cs = !nen;
   always_comb we = cs && !nwrite;
-  always_comb re = cs && nwrite;
   always_comb addr = {a0,a1,a2,a3};
   always_comb {q0,q1,q2,q3} = q;
   always_comb d = {d0, d1, d2, d3};
 
-  always_ff @(cs, re, we) begin
+  always_ff @(cs, we) begin
 
     if (cs) begin
 
       if (we) begin
 	ram[addr] <= d;
 	q <= '0;
-      end else if (re)
+      end else
 	q <= ram[addr];
-      else
-	q <= '0;
 
     end else
       q <= '0;
