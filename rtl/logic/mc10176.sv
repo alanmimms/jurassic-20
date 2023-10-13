e51f29dabb9fb10395fa88c1d104e1ba398de54d
Alan Mimms <alanmimms@gmail.com>
2023-10-13 13:54:37 -0700

    Rewrite to make this a true hex M/S FF implementation

--- rtl/logic/mc10176.sv
+++ rtl/logic/mc10176.sv
@@ -1,12 +1,17 @@
 module mc10176(input bit d0,d1,d2,d3,d4,d5, clk,
 	       output bit q0,q1,q2,q3,q4,q5);
+  bit [0:5] d, q, mid;
 
-  always_ff @(posedge clk) begin
-    q0 <= d0;
-    q1 <= d1;
-    q2 <= d2;
-    q3 <= d3;
-    q4 <= d4;
-    q5 <= d5;
+  always_comb begin
+    d = {d0,d1,d2,d3,d4,d5};
+    {q0,q1,q2,q3,q4,q5} = q;
   end
-endmodule
+
+  dff m[0:5] (.d(d), .clk(!clk), .q(mid));
+  dff s[0:5] (.d(mid), .clk(clk), .q(q));
+endmodule // mc10176
+
+
+module dff(input bit d, clk, output bit q);
+  always @(posedge clk) q <= d;
+endmodule // dff
