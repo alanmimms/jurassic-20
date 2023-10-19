6f324da616e88b3f9d4a166e21300c21ad11b3c0
Alan Mimms <alanmimms@gmail.com>
2023-10-18 22:09:04 -0700

    Slavery is wrong, and so is my master/slave DFF implementation

--- rtl/logic/mc10176.sv
+++ rtl/logic/mc10176.sv
@@ -1,17 +1,11 @@
 module mc10176(input bit d0,d1,d2,d3,d4,d5, clk,
 	       output bit q0,q1,q2,q3,q4,q5);
-  bit [0:5] d, q, mid;
+  bit [0:5] d, q;
 
   always_comb begin
     d = {d0,d1,d2,d3,d4,d5};
     {q0,q1,q2,q3,q4,q5} = q;
   end
 
-  dff m[0:5] (.d(d), .clk(!clk), .q(mid));
-  dff s[0:5] (.d(mid), .clk(clk), .q(q));
-endmodule // mc10176
-
-
-module dff(input bit d, clk, output bit q);
   always @(posedge clk) q <= d;
-endmodule // dff
+endmodule // mc10176
