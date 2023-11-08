23005ba25842da4399bd1dc3c3eb6a88a37ada2d
Alan Mimms <alanmimms@gmail.com>
2023-11-08 11:19:36 -0800

    Forgot to complement output of last XOR stage - duh

--- rtl/logic/mc10181.sv
+++ rtl/logic/mc10181.sv
@@ -35,16 +35,16 @@ module mc10181(input bit s8,s4,s2,s1, boole, cin,
     G = ~(~({4{s8}} | B | A) | ~({4{s4}} | A | ~B));
     P = ~(~(~B | {4{s2}}) | ~({4{s1}} | B) | ~A);
 
-    f8 = G[3] ^ P[3] ^ (~(boole | G[2]) |
-			~(boole | P[2] | G[1]) |
-			~(boole | P[2] | P[1] | G[0]) |
-			~(boole | P[2] | P[1] | P[0] | cin));
-    f4 = G[2] ^ P[2] ^ (~(boole | G[1]) |
-			~(boole | P[1] | G[0]) |
-			~(boole | P[1] | P[0] | cin));
-    f2 = G[1] ^ P[1] ^ (~(boole | G[0]) |
-			~(boole | P[0] | cin));
-    f1 = G[0] ^ P[0] ^ (~(boole | cin));
+    f8 = ~(G[3] ^ P[3] ^ (~(boole | G[2]) |
+			  ~(boole | P[2] | G[1]) |
+			  ~(boole | P[2] | P[1] | G[0]) |
+			  ~(boole | P[2] | P[1] | P[0] | cin)));
+    f4 = ~(G[2] ^ P[2] ^ (~(boole | G[1]) |
+			  ~(boole | P[1] | G[0]) |
+			  ~(boole | P[1] | P[0] | cin)));
+    f2 = ~(G[1] ^ P[1] ^ (~(boole | G[0]) |
+			  ~(boole | P[0] | cin)));
+    f1 = ~(G[0] ^ P[0] ^ (~(boole | cin)));
 
     notGG = ~G[3]  |
             ~(P[3] | G[2]) |
@@ -56,51 +56,3 @@ module mc10181(input bit s8,s4,s2,s1, boole, cin,
     cout = ~(notGG | ~(cp | cin));  
   end
 endmodule // mc10181
-
-
-`ifdef TESTBENCH
-module mc10181_tb;
-
-  bit clk;
-  bit [3:0] S, A, B;
-  bit [3:0] F;
-  bit M, cin;
-  bit CG, CP, COUT;
-  bit [31:0] s, a, b;
-  bit [31:0] m, cin;
-
-  mc10181 mc10181(.S(S), .M(M), .A(A), .B(B), .cin(cin), .F(F), .CG(CG), .CP(CP), .COUT(COUT));
-
-  always #1 clk = ~clk;
-
-  initial begin
-    clk = 0;
-    cin = 0;
-
-    for (m = 0; m < 2; m = m + 1) begin
-      M = m;
-
-      for (s = 0; s < 16; s = s + 1) begin
-        S = s;
-
-        for (b = 0; b < 16; b = b + 1) begin
-          B = b;
-
-          for (a = 0; a < 16; a = a + 1) begin
-            #1 A = a;
-            cin = ~cin;
-            cin = cin;
-          end
-        end
-      end
-    end
-  end
-
-  initial begin
-    $monitor("T=%-6D M=%b S=%4b A=%4b B=%4b cin=%b F=%4b CG=%b CP=%b COUT=%b",
-             $time, M, S, A, B, cin, F, CG, CP, COUT);
-    $dumpfile("mc10181_tb.vcd");
-    $dumpvars(0, mc10181_tb);
-  end
-endmodule // mc10181_tb
-`endif //  `ifdef TESTBENCH
--- rtl/tb/mc10181-tb.cc
+++ rtl/tb/mc10181-tb.cc
@@ -9,13 +9,20 @@ vluint64_t simT = 0;
 
 int main(int argc, char** argv, char** env) {
   int Fbit = 0;
-  static const unsigned MAX_SIM_TIME = (1ull << Fbit) + 3;
+  static const unsigned MAX_SIM_TIME = 1000;
   Vmc10181 *dut = new Vmc10181;
 
   Verilated::traceEverOn(true);
   VerilatedVcdC *m_trace = new VerilatedVcdC;
   dut->trace(m_trace, 5);
-  m_trace->open("waveform.vcd");
+  m_trace->open("mc10181-tb.vcd");
+
+  // Test 1100 BOOLE=1 case: Should result in 0000.
+  dut->s8 = 1;
+  dut->s4 = 1;
+  dut->s2 = 0;
+  dut->s1 = 0;
+  dut->boole = 1;
 
   while (simT < MAX_SIM_TIME) {
     dut->eval();
