85485fded18f7b69e440382cb10c9f49b66e61fc
Alan Mimms <alanmimms@gmail.com>
2023-11-12 21:01:54 -0800

    Memory should output its addressed location always when CE

--- rtl/logic/mc10147.sv
+++ rtl/logic/mc10147.sv
@@ -6,24 +6,15 @@ module mc10147(input bit a0, a1, a2, a3, a4, a5, a6,
   bit [6:0] addr;
   bit cs, we, re;
 
-  always_comb cs = !nen1 && !nen2;
-  always_comb we = cs && !nwrite;
-  always_comb re = cs && nwrite;
-  always_comb addr = {a0,a1,a2,a3,a4,a5,a6};
+  always_comb begin
+    cs = !nen1 && !nen2;
+    we = cs && !nwrite;
+    re = cs && nwrite;
+    addr = {a0,a1,a2,a3,a4,a5,a6};
+    q = re ? ram[addr] : 0;
+  end
 
-  always_ff @(cs, re, we) begin
-
-    if (cs) begin
-
-      if (we) begin
-	ram[addr] <= d;
-	q <= '0;
-      end else if (re)
-	q <= ram[addr];
-      else
-	q <= '0;
-
-    end else
-      q <= '0;
+  always_ff @(we) begin
+    if (we) ram[addr] <= d;
   end
 endmodule
