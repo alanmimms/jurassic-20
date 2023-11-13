44183eff1fa366a47aba173375220e7fb31d22aa
Alan Mimms <alanmimms@gmail.com>
2023-11-12 22:10:06 -0800

    Implement as a latch, but also with proper selector polarity!

--- rtl/logic/mc10173.sv
+++ rtl/logic/mc10173.sv
@@ -1,12 +1,15 @@
-module mc10173(input bit d00,d10,d20,d30, d01,d11,d21,d31,
-	       sel, nen,
+module mc10173(input bit d00,d10,d20,d30, d01,d11,d21,d31, sel, nen,
 	       output bit b0,b1,b2,b3);
+  bit hold = nen;
 
-  bit [3:0] curState;
-  bit [3:0] selState;
+  always_latch
 
-  always_ff @(nen) curState = sel ? {d00,d10,d20,d30} : {d01,d11,d21,d31};
-  always_comb selState = sel ? {d00,d10,d20,d30} : {d01,d11,d21,d31};
-  always_comb {b0,b1,b2,b3} = nen ? curState : selState;
+    if (!hold) begin
 
+      if (sel)
+	{b0,b1,b2,b3} = {d01,d11,d21,d31};
+      else
+	{b0,b1,b2,b3} = {d00,d10,d20,d30};
+
+    end
 endmodule
