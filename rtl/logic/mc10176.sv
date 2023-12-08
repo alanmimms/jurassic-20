d33fdc7d372b167728a4cba82ad1df5645cd6e42
Alan Mimms <alanmimms@gmail.com>
2023-12-07 21:21:43 -0800

    OCD cleanup

--- rtl/logic/mc10176.sv
+++ rtl/logic/mc10176.sv
@@ -1,11 +1,7 @@
 module mc10176(input bit d0,d1,d2,d3,d4,d5, clk,
 	       output bit q0,q1,q2,q3,q4,q5);
   bit [0:5] d, q;
-
-  always_comb begin
-    d = {d0,d1,d2,d3,d4,d5};
-    {q0,q1,q2,q3,q4,q5} = q;
-  end
-
+  always_comb d = {d0,d1,d2,d3,d4,d5};
   always @(posedge clk) q <= d;
+  always_comb {q0,q1,q2,q3,q4,q5} = q;
 endmodule // mc10176
