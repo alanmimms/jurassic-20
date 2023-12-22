d7fdb05ed198d0a4f08165b3a0606cf629a2e317
Alan Mimms <alanmimms@gmail.com>
2023-12-21 19:35:49 -0800

    Why so wordy? Simplify. Does not change resulting functionality.

--- rtl/logic/mc10176.sv
+++ rtl/logic/mc10176.sv
@@ -1,7 +1,4 @@
 module mc10176(input bit d0,d1,d2,d3,d4,d5, clk,
 	       output bit q0,q1,q2,q3,q4,q5);
-  bit [0:5] d, q;
-  always_comb d = {d0,d1,d2,d3,d4,d5};
-  always @(posedge clk) q <= d;
-  always_comb {q0,q1,q2,q3,q4,q5} = q;
+  always @(posedge clk) {q0,q1,q2,q3,q4,q5} <= {d0,d1,d2,d3,d4,d5};
 endmodule // mc10176
