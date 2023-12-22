789fcc12378460e1b385de5ec16a4d5a3349146c
Alan Mimms <alanmimms@gmail.com>
2023-12-22 11:42:38 -0800

    This is more readable and maybe different? It matches Signetics definition

--- rtl/logic/mc10121.sv
+++ rtl/logic/mc10121.sv
@@ -3,7 +3,15 @@ module mc10121(input bit a1, a2, a3, b1, b2, b3c3, c1, c2, d1, d2, d3,
 	       output bit qa, nqa);
   
   always_comb begin
-    qa = (a1 | a2 | a3) & (b1 | b2 | b3c3) & (c1 | c2 | b3c3) & (d1 | d2 | d3);
-    nqa = !qa;
+
+    qa = (a1 | a2 | a3) &
+	 (b1 | b2 | b3c3) &
+	 (c1 | c2 | b3c3) &
+	 (d1 | d2 | d3);
+
+    nqa = ~(a1 | a2 | a3) |
+	  ~(b1 | b2 | b3c3) |
+	  ~(c1 | c2 | b3c3) |
+	  ~(d1 | d2 | d3);
   end
 endmodule // mc10121
